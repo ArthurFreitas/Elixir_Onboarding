fd624cca6a9f2a728448b1c8a75595700b2fb74d
Arthur Freitas <bfreitas.arthur@gmail.com>
2020-09-15 20:17:20 -0300

    Adiciona dados mockados a view de show product

--- ims/lib/ims_web/controllers/product_controller.ex
+++ ims/lib/ims_web/controllers/product_controller.ex
@@ -1,26 +1,27 @@
 defmodule ImsWeb.ProductController do
   use ImsWeb, :controller
+  @products_mock ([%{SKU: "sku", name: "nome", description: "description", quantity: 15, price: 150.9 }])
 
   def index(conn, _params) do
-    render(conn, :list, products: [%{SKU: "sku", name: "nome", description: "description", quantity: 15, price: 150.9 }])
+    render(conn, :list, products: @products_mock)
   end
 
   def show(conn, %{"id" => id}) do
-    render(conn, :show, id: id)
+    render(conn, :show, id: id, product: hd(@products_mock))
   end
 
   def new(conn, _params) do
   end
 
-  def edit(conn, _params) do
+  def edit(conn, %{"id" => id}) do
   end
 
   def create(conn, _params) do
   end
 
-  def update(conn, _params) do
+  def update(conn, %{"id" => id}) do
   end
 
-  def destroy(conn, _params) do
+  def destroy(conn, %{"id" => id}) do
   end
 end
