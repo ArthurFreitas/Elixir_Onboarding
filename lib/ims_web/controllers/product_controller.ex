4f9d783883de34469837259c0a4d4010f37c75e8
Arthur Freitas <bfreitas.arthur@gmail.com>
2020-09-22 14:42:05 -0300

    Adiciona logging para o elastic search nos endpoints de product

--- lib/ims_web/controllers/product_controller.ex
+++ lib/ims_web/controllers/product_controller.ex
@@ -3,17 +3,21 @@ defmodule ImsWeb.ProductController do
   alias Ims.ProductHelper
   alias Ims.Product
   alias Ims.RedisHelper
+  alias Ims.ElasticSearchHelper
 
   def index(conn, _params) do
     render(conn, :list, products: ProductHelper.list)
   end
 
   def show(conn, %{"id" => id}) do
-    render(conn, :show, id: id, product: handleProductQuery(conn, id))
+    product = handleProductQuery(conn, id)
+    ElasticSearchHelper.post(:product, :show, conn, product)
+    render(conn, :show, id: id, product: product)
   end
 
   def new(conn, _params) do
     changeset = Product.changeset(%Product{}, %{})
+    ElasticSearchHelper.post(:product, :new, conn)
     render(conn, :new, changeset: changeset)
   end
 
@@ -21,22 +25,26 @@ defmodule ImsWeb.ProductController do
     RedisHelper.del(id)
     product = handleProductQuery(conn, id)
     changeset = Product.changeset(product,%{})
+    ElasticSearchHelper.post(:product, :edit, conn, product)
     render(conn, :edit, changeset: changeset, product: product)
   end
 
   def create(conn, %{"product" => product}) do
     ProductHelper.insert(product)
+    ElasticSearchHelper.post(:product, :create, conn, product)
     redirectToIndex(conn)
   end
 
   def update(conn, %{"id" => id, "product" => updatedProduct}) do
     product = handleProductQuery(conn, id)
     ProductHelper.update(product, updatedProduct)
+    ElasticSearchHelper.post(:product, :update, conn, product)
     redirectToIndex(conn)
   end
 
-  def destroy(conn, %{"id" => id}) do
+  def destroy(conn, %{"id" => id} = idMap) do
     ProductHelper.delete(id)
+    ElasticSearchHelper.post(:product, :destroy, conn, idMap)
     redirectToIndex(conn)
   end
 
