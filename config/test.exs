c764c7b535af70b9d2ebdc84007d467aae49967b
Arthur Freitas <bfreitas.arthur@gmail.com>
2020-09-23 11:14:44 -0300

    Adiciona configs do banco de teste

--- config/test.exs
+++ config/test.exs
@@ -6,11 +6,10 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :ims, Ims.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "ims_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "ims_test",
   hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
