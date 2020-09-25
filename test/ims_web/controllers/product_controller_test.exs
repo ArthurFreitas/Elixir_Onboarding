037eaaa7c6f8b30b815d5945fb623a61a874b83d
Arthur Freitas <bfreitas.arthur@gmail.com>
2020-09-25 01:26:12 -0300

    Adiciona teste de criar um novo produto de forma bem sucedida

--- test/ims_web/controllers/product_controller_test.exs
+++ test/ims_web/controllers/product_controller_test.exs
@@ -1,14 +1,36 @@
 defmodule ImsWeb.ProductControllerTest do
+  use ExUnit.Case, async: false
+  use Ims.DataCase, async: false
   use ImsWeb.ConnCase
-  use Ims.DataCase
 
   @moduletag :integration
 
+  @valid_product_attrs %{
+    SKU: "AbC-12",
+    name: "Abacate",
+    price: 15.0,
+    barcode: "12345678"
+  }
+
   describe "index" do
-    test "list all products", %{conn: conn} do
+    test "redirects to stock page", %{conn: conn} do
       conn = get(conn, Routes.product_path(conn, :index))
 
       assert html_response(conn, 200) =~ "Stock"
+    end
+  end
+
+  describe "create product" do
+    test "redirects to index and lists new product", %{conn: conn} do
+      conn = post(conn, Routes.product_path(conn, :create), product: @valid_product_attrs)
+
+      assert redirected_to(conn) == "/product"
+
+      conn = get(conn, Routes.product_path(conn, :index))
+      assert html_response(conn, 200) =~ @valid_product_attrs.'SKU'
+      assert html_response(conn, 200) =~ to_string(@valid_product_attrs.price)
+      assert html_response(conn, 200) =~ @valid_product_attrs.name
+      assert html_response(conn, 200) =~ @valid_product_attrs.barcode
     end
   end
 end
